bed45a1ed0f20b98d15b810fed14d1e02cd7845e
urshofer <u.hofer@tr51.org>
2021-04-01 18:20:27 +0200

    cont. dev

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+npm run build
+cd dist
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:urshofer/vue-paint.git master:gh-pages
+cd -
