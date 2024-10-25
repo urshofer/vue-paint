dbdbab10a9c7fb50a1be2cb465a0876772dc0f02
urshofer <u.hofer@tr51.org>
2024-10-25 17:45:58 +0200

    (fix) deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ set -e
 npm run build
 cd dist
 git init
+git checkout -b master  # create master branch explicitly if not present
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:urshofer/vue-paint.git master:gh-pages
